d4529c441ba806775d2bac01dec44c0afc8a9a25
Mingx1994x <mggxmingx@gmail.com>
2026-08-11 18:13:04 +0800

    feat: 統一錯誤處理改用 http-errors
    404 與其餘錯誤統一透過 next(err) 交給錯誤處理 middleware，
    並依 err.expose 決定是否將錯誤訊息回傳給前端。

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const createHttpError = require('http-errors');
+
 const AppDataSource = require('./db/data-source');
 
 // 建立 App
 const app = express();
-
 app.use(cors());
 
 // 定義 Route
@@ -16,6 +17,18 @@ app.get('/healthcheck', async (req, res) => {
     console.error(error);
     res.status(500).send('server is not working');
   }
-})
+});
+
+app.use((req, res, next) => {
+  next(createHttpError(404, '無此路由'));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.status || err.statusCode || 500;
+  res.status(statusCode).json({
+    status: statusCode === 500 ? "error" : "failed",
+    message: err.expose ? err.message : '伺服器錯誤'
+  })
+});
 
 module.exports = app;
